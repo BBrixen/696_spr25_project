8967a6785d00c7daf18fa2f96a5dbd8cf740e031
BBrixen <bbrixen02@gmail.com>
2025-03-15 14:38:23 -0700

    adds llm_interaction file to handle between different models

---
+++ llm_interaction.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+from cacher import cache
+from llama_cpp import Llama
+
+# handling apis
+from api_keys import openai_api_key
+client = OpenAI(api_key=openai_api_key)
+
+
+def ask_llm(prompt, local=True):
+    if local:
+        return ask_llama(prompt)
+    else:
+        return ask_openai(prompt)
+
+
+@cache
+def ask_llama(prompt):
+    llm = Llama(model_path="./models/codellama-13b.Q3_K_S.gguf")
+    output = llm(prompt)
+    return output["choices"][0]["text"]
+
+
+@cache
+def ask_openai(prompt):
+    messages = [ 
+            {
+                "role": "user", 
+                "content": prompt
+            }
+        ]
+
+    chat = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
+    reply = chat.choices[0].message.content
+    return reply
